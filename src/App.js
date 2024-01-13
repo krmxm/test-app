be36699391630a5db8895b1b786b15a294de3a97
Kramarenko Maxim <kramarenko-90@inbox.ru>
2024-01-13 19:27:41 +0300

    some commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+class WhoAmI extends Component { // возможнсть работать с пропсами нам приходит от реакт компонента от которого мы наследуем WhoAmI
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27
+    }
+  }
+
+  nextYear = () => {
+    console.log('+++');
+    // this.setState({
+    //   years: this.state.years + 1
+    // })
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    return (
+      <div className="App">
+        <button onClick={this.nextYear}>+++</button>
+        <h1>My name is {name }, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <WhoAmI name="John" surname="Smith" link="vk.com"/>
     </div>
-  );
+  )
 }
 
 export default App;
